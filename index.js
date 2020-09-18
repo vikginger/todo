8d7f66e8beba34dab940c3ce5269c6cb9eda6c9f
Ви <victorro1407@gmail.com>
2020-09-18 09:17:36 +0300

    Продолжаю программировать кнопки на JS

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let inputTask = document.getElementById('new-task');
+let blockToggleAll = document.querySelector('div.chekbox__toggle-all');
 let toggleAll = document.getElementById('toggle-all');
 let taskList = document.getElementById('todo-list');
+let blockFooter = document.querySelector('footer.footer');
 let countLeft = document.getElementById('todo-count-left');
 countLeft = 0;
 let filterAll = document.getElementById('filter-all');
@@ -38,6 +40,8 @@ function addTask() {
     taskList.appendChild(taskItem);
     bindTaskEvents(taskItem);
     inputTask.value="";
+    blockToggleAll.style.display = "block";
+    blockFooter.style.display = "flex";
   };
 };
 
@@ -53,6 +57,11 @@ function deleteTask() {
   let taskItem = this.parentNode;
   let taskList = taskItem.parentNode;
   taskList.removeChild(taskItem);
+
+  if (!taskList.hasChildNodes()) {
+    blockToggleAll.style.display = "none";
+    blockFooter.style.display = "none";
+  };
 };
 
 // удаление всех завершенных задач
@@ -62,6 +71,11 @@ clearCompleted.onclick = function deleteCompletedTasks() {
     if (taskItems[i].classList.contains('completed')) {
       taskItems[i].parentNode.removeChild(taskItems[i]);
     };
+  };
+
+  if (!taskList.hasChildNodes()) {
+    blockToggleAll.style.display = "none";
+    blockFooter.style.display = "none";
   };
 };
 
@@ -78,6 +92,20 @@ function toggleTask() {
     taskItem.classList.add('completed');
   } else {
     taskItem.classList.remove('completed');
+  };
+
+  //общий чек-лист загорается, если отмечены все задачи
+  let taskItems = this.parentNode.parentNode.querySelectorAll('li');
+  let countCompleted = 0;
+  for(let i = 0; i < taskItems.length; i++) {
+    if (taskItems[i].classList.contains('completed')) {
+      countCompleted ++;
+    };
+  };
+  if (countCompleted === taskItems.length) {
+    toggleAll.checked = true;
+  } else {
+    toggleAll.checked = false;
   };
 };
 
@@ -96,11 +124,52 @@ toggleAll.onclick = function toggleAllTasks () {
   };
 };
 
-
 function bindTaskEvents(taskItem) {
   let checkbox = taskItem.querySelector('input.toggle-one');
   let deleteButton = taskItem.querySelector('button.delete');
 
   checkbox.onclick = toggleTask;
   deleteButton.onclick = deleteTask;
+};
+
+
+// фильтрация задач
+
+//All
+filterAll.onclick = function filterAllTasks () {
+  let taskItems = taskList.querySelectorAll('li');
+  for(let i = 0; i < taskItems.length; i++) {
+    taskItems[i].style.display = "block";
+  };
+  filterActive.classList.remove('selected');
+  filterAll.classList.add('selected');
+  filterCompleted.classList.remove('selected');
+};
+
+//Active
+filterActive.onclick = function filterActiveTasks () {
+  let taskItems = taskList.querySelectorAll('li');
+  for(let i = 0; i < taskItems.length; i++) {
+    taskItems[i].style.display = "block";
+    if (taskItems[i].classList.contains('completed')) {
+      taskItems[i].style.display = "none";
+    };
+  };
+  filterActive.classList.add('selected');
+  filterAll.classList.remove('selected');
+  filterCompleted.classList.remove('selected');
+};
+
+//Completed
+filterCompleted.onclick = function filterCompletedTasks () {
+  let taskItems = taskList.querySelectorAll('li');
+  for(let i = 0; i < taskItems.length; i++) {
+    taskItems[i].style.display = "block";
+    if (!taskItems[i].classList.contains('completed')) {
+      taskItems[i].style.display = "none";
+    };
+  };
+  filterActive.classList.remove('selected');
+  filterAll.classList.remove('selected');
+  filterCompleted.classList.add('selected');
 };
