dfa2d953b971ddc466168329a707daf60d3b4578
Ви <victorro1407@gmail.com>
2020-09-21 10:37:12 +0300

    Задачи сохраняются после перезагрузки страницы и браузера

--- index.js
+++ index.js
@@ -9,10 +9,75 @@ let filterActive = document.getElementById('filter-active');
 let filterCompleted = document.getElementById('filter-completed');
 let clearCompleted = document.getElementById('clear-completed');
 
+let newId = 0;
+
+// счётчик
+function countLeftNumber() {
+  let count = 0;
+  let taskItemsList = taskList.querySelectorAll('li');
+  for(let i = 0; i < taskItemsList.length; i++) {
+    if (!taskItemsList[i].classList.contains('completed')) {
+      count++;
+    };
+  };
+  countLeft.innerHTML = count;
+};
+
+// добавление задач из LocalStorage при загрузке
+function getValueAfterReload() {
+  if (localStorage.length > 0) {
+		for (i = 0; i < localStorage.length; i++) {
+		  let key = localStorage.key(i);
+      let idTask = key.replace(/taskId_/gi, '');
+      let taskObject = JSON.parse(localStorage.getItem(key));
+      let valueTask = taskObject[0];
+      let classTask = taskObject[1];
+      let taskItem = createNewElement(valueTask, idTask);
+      taskItem.classList.add(classTask);
+      let toggleOne = taskItem.querySelector('input.toggle-one');
+      if (classTask === 'completed') {
+        toggleOne.checked = true;
+      } else {
+        toggleOne.checked = false;
+      };
+      taskList.appendChild(taskItem);
+      bindTaskEvents(taskItem);
+		};
+
+    let taskItems = taskList.querySelectorAll('li');
+    let countNotCompleted = 0;
+    let countCompleted = 0;
+    for(let i = 0; i < taskItems.length; i++) {
+      if (!taskItems[i].classList.contains('completed')) {
+        countNotCompleted ++;
+      };
+      if (taskItems[i].classList.contains('completed')) {
+        countCompleted ++;
+      };
+    };
+    if (countNotCompleted === taskItems.length) {
+      clearCompleted.style.display = "none";
+    } else {
+      clearCompleted.style.display = "block";
+    };
+    if (countCompleted === taskItems.length) {
+      toggleAll.checked = true;
+    } else {
+      toggleAll.checked = false;
+    };
+    blockToggleAll.style.display = "block";
+    blockFooter.style.display = "flex";
+
+    countLeftNumber();
+	};
+};
+
+getValueAfterReload();
 
 // Создание элемента списка
-function createNewElement (task) {
+function createNewElement (task, id) {
   let taskItem = document.createElement('li');
+  taskItem.setAttribute('data-item', id);
   let toggleOne = document.createElement('input');
   toggleOne.type = 'checkbox';
   toggleOne.className = 'toggle-one';
@@ -39,9 +104,11 @@ function createNewElement (task) {
 // Добавление новой задачи в список
 function addTask() {
   if (inputTask.value) {
-    let taskItem = createNewElement(inputTask.value);
+    let taskItem = createNewElement(inputTask.value, newId);
     taskList.appendChild(taskItem);
     bindTaskEvents(taskItem);
+    let taskLocalStorage = [inputTask.value];
+    localStorage.setItem('taskId_' + newId, JSON.stringify(taskLocalStorage));
     inputTask.value="";
     blockToggleAll.style.display = "block";
     blockFooter.style.display = "flex";
@@ -51,16 +118,17 @@ function addTask() {
 inputTask.addEventListener('keyup', function (e) {
   if (e.keyCode === 13) {
     event.preventDefault();
-    addTask();
-    // счётчик
     let taskItems = taskList.querySelectorAll('li');
-    let count = 0;
     for(let i = 0; i < taskItems.length; i++) {
-      if (!taskItems[i].classList.contains('completed')) {
-        count++;
+      let maxId = taskItems[i].getAttribute('data-item');
+      if (maxId > newId) {
+        newId = maxId;
       };
     };
-    countLeft.innerHTML = count;
+    newId++;
+    addTask();
+    // счётчик
+    countLeftNumber();
 
     if (taskItems.length === 1) {
       toggleAll.checked = false;
@@ -72,11 +140,27 @@ inputTask.addEventListener('keyup', function (e) {
 function deleteTask() {
   let taskItem = this.parentNode;
   let taskList = taskItem.parentNode;
+  localStorage.removeItem("taskId_" + taskItem.getAttribute('data-item'));
   taskList.removeChild(taskItem);
 
   if (!taskList.hasChildNodes()) {
     blockToggleAll.style.display = "none";
     blockFooter.style.display = "none";
+  };
+  countLeftNumber();
+
+  // убираем ClearCompleted, если нет завершенных задач
+  let taskItems = taskList.querySelectorAll('li');
+  let countNotCompleted = 0;
+  for(let i = 0; i < taskItems.length; i++) {
+    if (!taskItems[i].classList.contains('completed')) {
+      countNotCompleted ++;
+    };
+  };
+  if (countNotCompleted === taskItems.length) {
+    clearCompleted.style.display = "none";
+  } else {
+    clearCompleted.style.display = "block";
   };
 };
 
@@ -85,6 +169,7 @@ clearCompleted.onclick = function deleteCompletedTasks() {
   let taskItems = taskList.querySelectorAll('li');
   for(let i = 0; i < taskItems.length; i++) {
     if (taskItems[i].classList.contains('completed')) {
+      localStorage.removeItem("taskId_" + taskItems[i].getAttribute('data-item'));
       taskItems[i].parentNode.removeChild(taskItems[i]);
     };
   };
@@ -93,6 +178,8 @@ clearCompleted.onclick = function deleteCompletedTasks() {
     blockToggleAll.style.display = "none";
     blockFooter.style.display = "none";
   };
+  countLeftNumber();
+  clearCompleted.style.display = "none";
 };
 
 // завершение задачи через чекбокс
@@ -102,11 +189,20 @@ function toggleTask() {
   let countCompleted = 0;
   let countNotCompleted = 0;
   let toggleOne = taskItem.querySelector('input.toggle-one');
+  let label = taskItem.querySelector('label.label-text');
+  let classItem;
+  let taskLocalStorage = [label.innerHTML];
   if (toggleOne.checked) {
     taskItem.classList.add('completed');
+    classItem = 'completed';
+    taskLocalStorage[1] = classItem;
   } else {
     taskItem.classList.remove('completed');
+    taskLocalStorage[1] = classItem;
   };
+
+  // перезаписываем значение в LocalStorage вместе с классом
+  localStorage["taskId_" + taskItem.getAttribute('data-item')] = JSON.stringify(taskLocalStorage);
 
   //добавление Clear Completed
   for(let i = 0; i < taskItems.length; i++) {
@@ -114,8 +210,6 @@ function toggleTask() {
       countNotCompleted ++;
     };
   };
-  console.log(countNotCompleted);
-  console.log(taskItems.length);
   if (countNotCompleted === taskItems.length) {
     clearCompleted.style.display = "none";
   } else {
@@ -134,40 +228,33 @@ function toggleTask() {
     toggleAll.checked = false;
   };
 
-  // счётчик
-  let count = 0;
-  for(let i = 0; i < taskItems.length; i++) {
-    if (!taskItems[i].classList.contains('completed')) {
-      count++;
-    };
-  };
-  countLeft.innerHTML = count;
+  countLeftNumber();
 };
 
 // завершение всех задач через чекбокс
 toggleAll.onclick = function toggleAllTasks () {
   let taskItems = taskList.querySelectorAll('li');
+  let classItem;
   for(let taskItem of taskItems) {
     let toggleOne = taskItem.querySelector('input.toggle-one');
+    let label = taskItem.querySelector('label.label-text');
+    let taskLocalStorage = [label.innerHTML];
     if (toggleAll.checked) {
       taskItem.classList.add('completed');
       toggleOne.checked = true;
+      classItem = 'completed';
+      taskLocalStorage[1] = classItem;
       clearCompleted.style.display = "block";
     } else {
       taskItem.classList.remove('completed');
       toggleOne.checked = false;
+      taskLocalStorage[1] = classItem;
       clearCompleted.style.display = "none";
     };
+    localStorage["taskId_" + taskItem.getAttribute('data-item')] = JSON.stringify(taskLocalStorage);
   };
 
-  // счётчик
-  let count = 0;
-  for(let i = 0; i < taskItems.length; i++) {
-    if (!taskItems[i].classList.contains('completed')) {
-      count++;
-    };
-  };
-  countLeft.innerHTML = count;
+  countLeftNumber();
 };
 
 function bindTaskEvents(taskItem) {
@@ -186,13 +273,20 @@ function bindTaskEvents(taskItem) {
     let input = taskItem.querySelector('input[type=text]');
     let toggleOne = taskItem.querySelector('input.toggle-one');
     let deleteButton = taskItem.querySelector('button.delete');
-
+    let taskLocalStorage = [];
+    let taskClass;
     taskItem.classList.add('editMode');
     input.value = editLabel.innerText;
 
     input.addEventListener('blur', function() {
 		  editLabel.innerText = this.value;
       taskItem.classList.remove('editMode');
+      taskLocalStorage[0] = this.value;
+      if (taskItem.classList.contains('completed')) {
+        taskClass = 'completed';
+      }
+      taskLocalStorage[1] = taskClass;
+      localStorage["taskId_" + taskItem.getAttribute('data-item')] = JSON.stringify(taskLocalStorage);
       editLabel.addEventListener('dblclick', func);
 	  });
 
